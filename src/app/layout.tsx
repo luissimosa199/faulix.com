8f36942ca96aed6870f9b7356272c67081d0498d
Luis <simosa37@gmail.com>
2024-01-22 16:26:07 -0300

    carrousel impl.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import "./globals.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/components/ClientsSection.tsx
+++ src/components/ClientsSection.tsx
@@ -1,19 +1,147 @@
+"use client";
 import React from "react";
 import SectionTitle from "./SectionTitle";
+import Slider from "react-slick";
+import Image from "next/image";
 
 const ClientsSection = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <section className="bg-gray-200 p-4 sm:px-48">
       <SectionTitle className="text-center">
         Contribuimos significativamente al avance y prosperidad de los negocios
         de las siguientes firmas
       </SectionTitle>
-
-      <div className="flex flex-wrap my-4">
-        <div className="w-1/2 h-32 bg-gray-300 border"></div>
-        <div className="w-1/2 h-32 bg-gray-300 border"></div>
-        <div className="w-1/2 h-32 bg-gray-300 border"></div>
-        <div className="w-1/2 h-32 bg-gray-300 border"></div>
+      <div className="px-4">
+        <Slider {...settings}>
+          <>
+            <div className="flex flex-wrap my-4">
+              <div className="w-1/2 h-32">
+                <Image
+                  className="mx-auto"
+                  src="/images/clients/ypf.png"
+                  alt="ypg logo"
+                  width={200}
+                  height={100}
+                />
+              </div>
+              <div className="w-1/2 h-32">
+                <Image
+                  className="mx-auto"
+                  src="/images/clients/victorpodesta.png"
+                  alt="victor podesta logo"
+                  width={200}
+                  height={100}
+                />
+              </div>
+              <div className="w-1/2 h-32">
+                <Image
+                  className="mx-auto"
+                  src="/images/clients/beconnected.png"
+                  alt="beconnected logo"
+                  width={200}
+                  height={100}
+                />
+              </div>
+              <div className="w-1/2 h-32">
+                <Image
+                  className="mx-auto"
+                  src="/images/clients/betfriend.png"
+                  alt="betfriend logo"
+                  width={200}
+                  height={100}
+                />
+              </div>
+            </div>
+          </>
+          <>
+            <div className="flex flex-wrap my-4">
+              <div className="w-1/2 h-32">
+                <Image
+                  className="mx-auto"
+                  src="/images/clients/asistenteai.png"
+                  alt="asistenteai logo"
+                  width={200}
+                  height={100}
+                />
+              </div>
+              <div className="w-1/2 h-32">
+                <Image
+                  className="mx-auto"
+                  src="/images/clients/religa.png"
+                  alt="religa logo"
+                  width={200}
+                  height={100}
+                />
+              </div>
+              <div className="w-1/2 h-32">
+                <Image
+                  className="mx-auto"
+                  src="/images/clients/openai.png"
+                  alt="openai logo"
+                  width={200}
+                  height={100}
+                />
+              </div>
+              <div className="w-1/2 h-32">
+                <Image
+                  className="mx-auto"
+                  src="/images/clients/mercadopago.png"
+                  alt="mercadopago logo"
+                  width={200}
+                  height={100}
+                />
+              </div>
+            </div>
+          </>
+          <>
+            <div className="flex flex-wrap my-4">
+              <div className="w-1/2 h-32">
+                <Image
+                  className="mx-auto"
+                  src="/images/clients/hectorjulian.png"
+                  alt="hectorjulian logo"
+                  width={200}
+                  height={100}
+                />
+              </div>
+              <div className="w-1/2 h-32">
+                <Image
+                  className="mx-auto"
+                  src="/images/clients/chatgpt.png"
+                  alt="chatgtp logo"
+                  width={200}
+                  height={100}
+                />
+              </div>
+              <div className="w-1/2 h-32">
+                <Image
+                  className="mx-auto"
+                  src="/images/clients/carlosgardenal.png"
+                  alt="carlosgardenal logo"
+                  width={200}
+                  height={100}
+                />
+              </div>
+              <div className="w-1/2 h-32">
+                <Image
+                  className="mx-auto"
+                  src="/images/clients/bosques.png"
+                  alt="bosques logo"
+                  width={200}
+                  height={100}
+                />
+              </div>
+            </div>
+          </>
+        </Slider>
       </div>
     </section>
   );
