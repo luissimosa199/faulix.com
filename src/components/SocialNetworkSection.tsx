1088113ec693bdd52d0767c9634f45a41d54802e
Luis <simosa37@gmail.com>
2024-01-22 13:44:41 -0300

    social desktop version

--- src/components/SocialNetworkSection.tsx
+++ src/components/SocialNetworkSection.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SocialNetworkSection = () => {
   return (
-    <section className="px-2 py-16">
+    <section className="px-2 py-16 sm:hidden">
       <h2 className="text-center mb-6">Comparte esta página</h2>
       <div className="flex gap-4 justify-center">
         <div className="w-16 h-16 rounded-full bg-gray-300"></div>
