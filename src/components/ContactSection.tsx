bbcf6602df57d9fac85c39dfa4c3fbc192cbad93
Luis <simosa37@gmail.com>
2024-01-22 13:38:52 -0300

    contacto desktop version

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -4,9 +4,9 @@ import SectionTitle from "./SectionTitle";
 
 const ContactSection = () => {
   return (
-    <section className="py-8">
+    <section className="py-8 sm:flex sm:justify-center">
       <SectionTitle className="text-center mb-12">Contactanos</SectionTitle>
-      <div className="flex flex-col gap-1 mx-2">
+      <div className="flex flex-col gap-1 mx-2 sm:flex-row">
         <Link href="#">
           <div className="bg-gray-700 text-gray-100 px-4 py-2 text-center text-lg rounded-sm">
             Futuros clientes
