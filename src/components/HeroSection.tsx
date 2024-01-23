82422d177027d84206aa26a38b305943f1d496f6
Luis <simosa37@gmail.com>
2024-01-23 13:46:47 -0300

    added hero text

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -20,15 +20,18 @@ const HeroSection = () => {
         />
       </div>
       <div className="mx-4 p-8 bg-gray-200 flex flex-col gap-2 absolute top-60 sm:max-w-[500px] lg:ml-48 sm:top-48 ">
-        <h2 className="font-semibold text-lg">Titulo 1</h2>
+        <h2 className="font-semibold text-lg">
+          Soluciones Digitales Personalizadas
+        </h2>
         <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga dolorum
-          saepe amet alias ea repudiandae id maxime ex ipsum iure nostrum, quia
-          iste quos, quasi maiores voluptatum voluptatibus quod quas adipisci
-          perferendis soluta repellat!
+          Faulix especializa en desarrollo de software a medida, ofreciendo
+          soluciones innovadoras aplicando inteligencia artificial en análisis y
+          automatización empresarial. Proporcionamos mantenimiento web continuo,
+          junto con la gestión eficiente de campañas publicitarias en
+          plataformas digitales.
         </p>
         <Link
-          href="#"
+          href="/contacto"
           className="text-orange-500 font-semibold flex gap-1 items-center"
         >
           <span>Saber más</span>
