1180acbedb372ca4f58edd0d92579b7c26c1e3a9
Luis <simosa37@gmail.com>
2024-01-23 18:58:05 -0300

    services uncommented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <HeroSection />
       {/* <IndustriesSection /> */}
       {/* <ClientsSection /> */}
-      {/* <ServicesSection /> */}
+      <ServicesSection />
       {/* <PortfolioSection /> */}
       <ContactSection />
     </main>
