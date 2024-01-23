c9f40d457f14d82b8a6e369a3959de0484189448
Luis <simosa37@gmail.com>
2024-01-23 16:21:45 -0300

    all commendted out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,12 @@ import ServicesSection from "@/components/ServicesSection";
 export default function Home() {
   return (
     <main className="flex flex-col gap-4">
-      <HeroSection />
-      <IndustriesSection />
-      <ClientsSection />
-      <ServicesSection />
-      <PortfolioSection />
-      <ContactSection />
+      {/* <HeroSection /> */}
+      {/* <IndustriesSection /> */}
+      {/* <ClientsSection />
+      <ServicesSection /> */}
+      {/* <PortfolioSection /> */}
+      {/* <ContactSection /> */}
     </main>
   );
 }
